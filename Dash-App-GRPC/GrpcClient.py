6919207de3389763dbf3b7a794143488f0a2c133
Himadri Talukder <htalukder@ibm.com>
2022-08-24 13:53:58 -0400

    updated proto model

--- Dash-App-GRPC/GrpcClient.py
+++ Dash-App-GRPC/GrpcClient.py
@@ -1,3 +1,10 @@
+# *****************************************************************#
+# (C) Copyright IBM Corporation 2022.                             #
+#                                                                 #
+# The source code for this program is not published or otherwise  #
+# divested of its trade secrets, irrespective of what has been    #
+# deposited with the U.S. Copyright Office.                       #
+# *****************************************************************#
 import os
 import grpc
 import syntax_types_pb2
@@ -14,32 +21,33 @@ class GrpcClient:
 
      # a method calling syntax_izumo_en_stock model
     def call_syntax_izumo(self, inputText):
-        request = CommonService.watson_nlp_blocks_syntax_izumo_IzumoTextProcessing_Request(
-            raw_document=syntax_types_pb2.RawDocument(text=inputText)
+        request = CommonService.SyntaxIzumoRequest(
+            raw_document=syntax_types_pb2.RawDocument(text=inputText),
+            parsers=syntax_types_pb2.SyntaxParserSpec(parsers=[syntax_types_pb2.SYNTAX_LEMMA])
         )   
         SYNTAX_IZUMO_EN_STOCK_MODEL = os.getenv("SYNTAX_IZUMO_EN_STOCK_MODEL", default="syntax-izumo-en-stock-predictor")
         print("###### Calling remote GRPC model = ", SYNTAX_IZUMO_EN_STOCK_MODEL)
-        response = self.stub.watson_nlp_blocks_syntax_izumo_IzumoTextProcessing_Predict(request, metadata=[("mm-vmodel-id", SYNTAX_IZUMO_EN_STOCK_MODEL)])
+        response = self.stub.SyntaxIzumoPredict(request, metadata=[("mm-vmodel-id", SYNTAX_IZUMO_EN_STOCK_MODEL)])
         return response
 
     # a method calling sentiment_document-cnn-workflow_en_stock
     def call_sentiment_document_workflow(self, inputText):
-        request = CommonService.watson_nlp_workflows_document_sentiment_cnn_CNN_Request(
+        request = CommonService.SentimentDocumentWorkflowRequest(
             raw_document=syntax_types_pb2.RawDocument(text=inputText),
             sentence_sentiment=True,
             show_neutral_scores=True
         )
         SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL = os.getenv("SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL", default="sentiment-document-cnn-workflow-en-stock-predictor")
         print("###### Calling remote GRPC model = ", SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL)
-        response = self.stub.watson_nlp_workflows_document_sentiment_cnn_CNN_Predict(request,metadata=[("mm-vmodel-id", SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL)] )
+        response = self.stub.SentimentDocumentWorkflowPredict(request,metadata=[("mm-vmodel-id", SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL)] )
         return response
 
     # emotion analysis ensemble_classification-wf_en_emotion-stock
     def call_emotion_model(self, inputText):
-        request = CommonService.watson_nlp_workflows_classification_ensemble_Ensemble_Request(
-            raw_document=inputText
+        request = CommonService.EmotionDocumentWorkflowRequest(
+            raw_document=syntax_types_pb2.RawDocument(text=inputText)
         )
         EMOTION_CLASSIFICATION_STOCK_MODEL = os.getenv("EMOTION_CLASSIFICATION_STOCK_MODEL", default="ensemble-classification-wf-en-emotion-stock-predictor")
         print("###### Calling remote GRPC model = ", EMOTION_CLASSIFICATION_STOCK_MODEL)
-        response = self.stub.watson_nlp_workflows_classification_ensemble_Ensemble_Predict(request,metadata=[("mm-vmodel-id", EMOTION_CLASSIFICATION_STOCK_MODEL)] )
+        response = self.stub.EmotionDocumentWorkflowPredict(request,metadata=[("mm-vmodel-id", EMOTION_CLASSIFICATION_STOCK_MODEL)] )
         return response
