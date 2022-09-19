072fd7580fc8fee6e3c9e4bc825503df9a22ea23
Himadri Talukder <htalukder@ibm.com>
2022-09-19 11:29:40 -0400

    updated default model

--- Watson-NLP-Custom-Model-Container/Client/GrpcClient.py
+++ Watson-NLP-Custom-Model-Container/Client/GrpcClient.py
@@ -21,11 +21,11 @@ class GrpcClient:
         self.stub = common_service_pb2_grpc.CommonServiceStub(channel)
 
     def call_nlp_model(self,inputText):
-        request = common_service_pb2.watson_nlp_topics_Message(
+        request = common_service_pb2.EmotionDocumentWorkflowRequest(
            raw_document=dm.RawDocument(text=inputText).to_proto()
         )
         
         WATSON_NLP_MODEL_ID = os.getenv("WATSON_NLP_MODEL_ID", default="ensemble_classification-wf_en_emotion-stock")
         print("###### Calling remote GRPC model = ", WATSON_NLP_MODEL_ID)
-        response = self.stub.watson_nlp_topics_Predict(request,metadata=[("mm-model-id", WATSON_NLP_MODEL_ID)] )
+        response = self.stub.EmotionDocumentWorkflowPredict(request,metadata=[("mm-model-id", WATSON_NLP_MODEL_ID)] )
         return response
