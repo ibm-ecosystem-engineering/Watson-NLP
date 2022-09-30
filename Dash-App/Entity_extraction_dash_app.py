f06a963cbf92917356b2d92b732940389d0c02e5
Christopher Chen <cchenchristopher@gmail.com>
2022-09-30 13:51:09 -0700

    Update navbar header Entity App

--- Dash-App/Entity_extraction_dash_app.py
+++ Dash-App/Entity_extraction_dash_app.py
@@ -59,43 +59,52 @@ navbar_main = dbc.Navbar(
                 dbc.Row(
                     [
                     dbc.Col(html.Img(src=app.get_asset_url('ibm_logo.png'), height="30px")),
-                    dbc.Col(dbc.NavbarBrand("Build Lab", className="me-auto")),
+                    dbc.Col(dbc.NavbarBrand("Build Lab", className="ml-auto")),
+                    #dbc.Col(html.H2("Watson NLP"), className="me-auto", justify='center')
                     ],
-                    align="center",
                     className="w-0",
                 ),
-                style={"textDecoration": "bold", "margin-right": "20%"},
+                style={"textDecoration": "bold", "margin-right": "30%"},
             ),
-            dbc.Row(html.H2("Watson NLP: "),
-            className="me-auto",
-            justify='center',
-            ),
-            dbc.Row(html.H3("Entity and Phrase Extraction"),
-            className="me-auto",
-            justify='center'
-            ),
-            dbc.Row(
+            dbc.Col(
                 [
-                        dbc.Nav(
-                            [
-                                dbc.NavItem(
-                                    dbc.NavLink("©"),
-                                    # add an auto margin after this to push later links to end of nav
-                                    className="me-auto",
-                                ),
-                                # html.Span(dcc.LogoutButton(logout_url='https://w3.ibm.com/w3publisher/ibm-build-labs'), className="ml-auto")
-                            ],
-                            # make sure nav takes up the full width for auto margin to get applied
-                            className="w-100",
-                        ),
+                    dbc.Row(
+                        [
+                            html.H2("Watson NLP ©", style={'textAlign': 'center'}),
+                            
+                        ],
+                        className="me-auto",
+                        align='center',
+                        justify='center',
+                    ),
+                    dbc.Row(html.H4("Entity and Phrase Extraction", style={'textAlign': 'center'}),
+                        className="me-auto",
+                        align='center',
+                        justify='center'
+                    ),
                 ],
-        className="flex-grow-1",
-        )
+                align = 'center'
+            ),
+            dbc.Col([]),
         ],
     color="primary",
     dark=True,
     className = "ml-auto"
 )
+'''
+                            dbc.Nav(
+                                [
+                                    dbc.NavItem(
+                                        dbc.NavLink("Watson NLP ©"),
+                                        # add an auto margin after this to push later links to end of nav
+                                        className="me-auto",
+                                    ),
+                                    # html.Span(dcc.LogoutButton(logout_url='https://w3.ibm.com/w3publisher/ibm-build-labs'), className="ml-auto")
+                                ],
+                            # make sure nav takes up the full width for auto margin to get applied
+                            className="w-100",
+                            ),
+                            '''
 
 entity_sample_text = 'I am so angry that i made this post available via all possible sites i use when planing my trips so no one will make the mistake of booking this place I made my booking via booking com We stayed for 6 nights in this hotel from 11 to 17 July Upon arrival we were placed in a small room on the 2nd floor of the hotel It turned out that this was not the room we booked I had specially reserved the 2 level duplex room so that we would have a big windows and high ceilings The room itself was ok if you don t mind the broken window that can not be closed hello rain and a mini fridge that contained some sort of a bio weapon at least i guessed so by the smell of it I intimately asked to change the room and after explaining 2 times that i booked a duplex btw it costs the same as a simple double but got way more volume due to the high ceiling was offered a room but only the next day SO i had to check out the next day before 11 o clock in order to get the room i waned to Not the best way to begin your holiday So we had to wait till 13 00 in order to check in my new room what a wonderful waist of my time The room 023 i got was just as i wanted to peaceful internal garden view big window We were tired from waiting the room so we placed our belongings and rushed to the city In the evening it turned out that there was a constant noise in the room i guess it was made by vibrating vent tubes or something it was constant and annoying as hell AND it did not stop even at 2 am making it hard to fall asleep for me and my wife I have an audio recording that i can not attach here but if you want i can send it via e mail The next day the technician came but was not able to determine the cause of the disturbing sound so i was offered to change the room once again the hotel was fully booked and they had only 1 room left the one that was smaller but seems newer '
 # entity_sample_text_box = dbc.InputGroupText("Copy Sample text for Entity Extraction")
