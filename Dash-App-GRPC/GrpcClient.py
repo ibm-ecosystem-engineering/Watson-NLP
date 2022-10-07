83989a79da4b278dc4581a87b334f697c359442c
Himadri Talukder <htalukder@ibm.com>
2022-10-07 17:22:25 -0400

    Emotion dash app added grpc

--- Dash-App-GRPC/GrpcClient.py
+++ Dash-App-GRPC/GrpcClient.py
@@ -1,30 +1,32 @@
-# *****************************************************************#
+#*****************************************************************#
 # (C) Copyright IBM Corporation 2022.                             #
 #                                                                 #
 # The source code for this program is not published or otherwise  #
 # divested of its trade secrets, irrespective of what has been    #
 # deposited with the U.S. Copyright Office.                       #
-# *****************************************************************#
-import os
+#*****************************************************************#
 import grpc
-import syntax_types_pb2
-import common_service_pb2_grpc as CommonServiceStub
-import common_service_pb2 as CommonService
+import os
+from watson_nlp_runtime_client import (
+    common_service_pb2,
+    common_service_pb2_grpc,
+    syntax_types_pb2
+)
 
 class GrpcClient:
 
-    NLP_MODEL_SERVICE_TYPE=os.getenv("NLP_MODEL_SERVICE_TYPE", default="mm-model-id")
+    NLP_MODEL_SERVICE_TYPE=os.getenv("NLP_MODEL_SERVICE_TYPE", default="mm-vmodel-id")
 
     # default constructor
     def __init__(self):
         GRPC_SERVER_URL = os.getenv("GRPC_SERVER_URL", default="localhost:8085")
         print("###### Calling GRPC endpoint = ", GRPC_SERVER_URL)
         channel = grpc.insecure_channel(GRPC_SERVER_URL)
-        self.stub = CommonServiceStub.CommonServiceStub(channel)
+        self.stub = common_service_pb2_grpc.NlpServiceStub(channel)
 
      # a method calling syntax_izumo_en_stock model
     def call_syntax_izumo(self, inputText):
-        request = CommonService.SyntaxIzumoRequest(
+        request =  common_service_pb2.SyntaxRequest(
             raw_document=syntax_types_pb2.RawDocument(text=inputText),
             parsers=syntax_types_pb2.SyntaxParserSpec(parsers=[syntax_types_pb2.SYNTAX_LEMMA])
         )   
@@ -35,22 +37,22 @@ class GrpcClient:
 
     # a method calling sentiment_document-cnn-workflow_en_stock
     def call_sentiment_document_workflow(self, inputText):
-        request = CommonService.SentimentDocumentWorkflowRequest(
+        request = common_service_pb2.SentimentRequest(
             raw_document=syntax_types_pb2.RawDocument(text=inputText),
-            sentence_sentiment=True,
-            show_neutral_scores=True
+        #    sentence_sentiment=True,
+        #    show_neutral_scores=True
         )
         SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL = os.getenv("SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL", default="sentiment_document-cnn-workflow_en_stock")
         print("###### Calling remote GRPC model = ", SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL)
-        response = self.stub.SentimentDocumentWorkflowPredict(request,metadata=[(self.NLP_MODEL_SERVICE_TYPE, SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL)] )
+        response = self.stub.SentimentPredict(request,metadata=[(self.NLP_MODEL_SERVICE_TYPE, SENTIMENT_DOCUMENT_CNN_WORKFLOW_MODEL)] )
         return response
 
     # emotion analysis ensemble_classification-wf_en_emotion-stock
     def call_emotion_model(self, inputText):
-        request = CommonService.EmotionDocumentWorkflowRequest(
+        request = common_service_pb2.SentimentRequest(
             raw_document=syntax_types_pb2.RawDocument(text=inputText)
         )
         EMOTION_CLASSIFICATION_STOCK_MODEL = os.getenv("EMOTION_CLASSIFICATION_STOCK_MODEL", default="ensemble_classification-wf_en_emotion-stock")
         print("###### Calling remote GRPC model = ", EMOTION_CLASSIFICATION_STOCK_MODEL)
-        response = self.stub.EmotionDocumentWorkflowPredict(request,metadata=[(self.NLP_MODEL_SERVICE_TYPE, EMOTION_CLASSIFICATION_STOCK_MODEL)] )
+        response = self.stub.ClassificationPredict(request,metadata=[(self.NLP_MODEL_SERVICE_TYPE, EMOTION_CLASSIFICATION_STOCK_MODEL)] )
         return response
