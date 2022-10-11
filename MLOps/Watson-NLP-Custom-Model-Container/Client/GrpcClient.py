b58e9e1a551bbe150b010582719f660a90152e78
Himadri Talukder <htalukder@ibm.com>
2022-10-11 14:25:37 -0400

    docker file updated and python custom client

--- MLOps/Watson-NLP-Custom-Model-Container/Client/GrpcClient.py
+++ MLOps/Watson-NLP-Custom-Model-Container/Client/GrpcClient.py
@@ -7,10 +7,10 @@
 #*****************************************************************#
 import grpc
 import os
-import watson_nlp.data_model as dm
 from watson_nlp_runtime_client import (
     common_service_pb2_grpc,
-    common_service_pb2
+    common_service_pb2,
+    syntax_types_pb2
 )
 class GrpcClient:
     # default constructor
@@ -18,14 +18,14 @@ class GrpcClient:
         GRPC_SERVER_URL = os.getenv("GRPC_SERVER_URL", default="localhost:8085")
         print("###### Calling GRPC endpoint = ", GRPC_SERVER_URL)
         channel = grpc.insecure_channel(GRPC_SERVER_URL)
-        self.stub = common_service_pb2_grpc.CommonServiceStub(channel)
+        self.stub = common_service_pb2_grpc.NlpServiceStub(channel)
 
     def call_nlp_model(self,inputText):
-        request = common_service_pb2.EmotionDocumentWorkflowRequest(
-           raw_document=dm.RawDocument(text=inputText).to_proto()
+        request = common_service_pb2.EmotionRequest(
+           raw_document=syntax_types_pb2.RawDocument(text=inputText),
         )
         
-        WATSON_NLP_MODEL_ID = os.getenv("WATSON_NLP_MODEL_ID", default="ensemble_classification-wf_en_emotion-stock")
+        WATSON_NLP_MODEL_ID = os.getenv("WATSON_NLP_MODEL_ID", default="ensemble_model")
         print("###### Calling remote GRPC model = ", WATSON_NLP_MODEL_ID)
-        response = self.stub.EmotionDocumentWorkflowPredict(request,metadata=[("mm-model-id", WATSON_NLP_MODEL_ID)] )
+        response = self.stub.ClassificationPredict(request,metadata=[("mm-model-id", WATSON_NLP_MODEL_ID)] )
         return response
