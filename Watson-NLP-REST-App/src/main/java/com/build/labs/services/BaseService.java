9a763faebb25f4e90832c403dbb24932ba548839
Michael Spriggs <mspriggs@ca.ibm.com>
2022-10-11 17:52:12 -0400

    migrating files under the MLOps subdir

--- Watson-NLP-REST-App/src/main/java/com/build/labs/services/BaseService.java
+++
@@ -1,9 +0,0 @@
-package com.build.labs.services;
-
-import com.build.labs.dto.CommonRequestDto;
-import com.build.labs.feign.FeignModelInferenceClient;
-
-public interface BaseService {
-	String MODEL_ID_KEY = "grpc-metadata-mm-model-id";
-	<T> T serve(CommonRequestDto commonRequest, FeignModelInferenceClient postFeignClient);
-}
