c7a3b6b8985e1295414acdf13d559b216356eebb
Daniel Krook <krook@us.ibm.com>
2022-11-03 15:15:50 -0400

    Removed linted space from comment.
    Signed-off-by: Daniel Krook <krook@us.ibm.com>

--- MLOps/Watson-NLP-Container/Client/GrpcClient.py
+++ MLOps/Watson-NLP-Container/Client/GrpcClient.py
@@ -1,10 +1,10 @@
-# *****************************************************************#
+#*****************************************************************#
 # (C) Copyright IBM Corporation 2022.                             #
 #                                                                 #
 # The source code for this program is not published or otherwise  #
 # divested of its trade secrets, irrespective of what has been    #
 # deposited with the U.S. Copyright Office.                       #
-# *****************************************************************#
+#*****************************************************************#
 import grpc
 import os
 from watson_nlp_runtime_client import (
